2e39166373cbad657c434f88e48164516ebe6942
deepthishivani <deepthishivani@gmail.com>
2026-02-24 11:45:17 +0530

    Week 3-4: Created Level 1, 2, 3 problem structure

---
+++ src/week_3_4/level1/problem6.java
@@ -0,0 +1,19 @@
+package level1;
+import java.util.Scanner;
+
+public class problem6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+
+        sc.close();
+    }
+}
